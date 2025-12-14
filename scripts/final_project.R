283369aace91bf08107760e141690d0eabf231d5
cmbappleby <129984364+cmbappleby@users.noreply.github.com>
2025-12-14 11:31:05 -0500

    Attempt to add locations

--- scripts/final_project.R
+++ scripts/final_project.R
@@ -1,13 +1,16 @@
 library(here)
 library(tidyverse)
 
+### ANALYSIS ###
 # https://irma.nps.gov/DataStore/Reference/Profile/2305079
 # Load plants lookup table
 tlu_plants <- read_csv(here('NCPN_IntegratedUplands_PlantsLookup.csv')) %>%
   select(c(Master_Plant_Code, Master_Common_Name))
 
 # Load NCPN point-intercept data and filter for Arches National Park
-arches_veg <- read_csv(here('data/NCPN_IntegratedUplands_PointIntercept.csv')) %>%
+arches_veg <- read_csv(
+  here('data/NCPN_IntegratedUplands_PointIntercept.csv')
+  ) %>%
   filter(Unit_Code == 'ARCH')
 
 # Save CSV so the data can be uploaded to GitHub
@@ -19,13 +22,23 @@ write.csv(arches_veg,
 # per transect, so the counts equals the percent cover.
 arches_counts <- arches_veg %>%
   filter(!is.na(Scientific_Name), Status == 1) %>%
-  select(Visit_Year:Layer, Species, Scientific_Name) %>%
-  distinct(Visit_Year, Plot_ID, Transect, Point, Species, Scientific_Name) %>%
-  group_by(Visit_Year, Plot_ID, Species, Scientific_Name) %>%
-  dplyr::summarize(
+  select(Start_Date:Layer, Species, Scientific_Name) %>%
+  distinct(Start_Date, Plot_ID, Transect, Point, Species, Scientific_Name) %>%
+  group_by(Start_Date, Plot_ID, Species, Scientific_Name) %>%
+  summarize(
     Count = n(),
     Cover_pct = round(Count / 300 * 100, 0),
     .groups = "drop"
+  ) %>%
+  mutate(
+    Cover_Class = cut(
+      Cover_pct,
+      breaks = c(0, 5, 25, 50, 75, 95, 100),
+      labels = c('1', '2', '3', '4', '5', '6'),
+      include.lowest = TRUE,
+      right = TRUE
+    ),
+    Cover_Class = as.numeric(Cover_Class)
   )
 
 # Calculate percent change for each species
@@ -48,11 +61,14 @@ plots_change <- plots_veg_cover %>%
   arrange(desc(Change_total)) %>%
   slice_head(n = 5)
 
+# Select the data for the top five plots with change
 arches_high_change <- arches_counts %>%
   filter(Plot_ID %in% plots_change$Plot_ID) %>%
-  arrange(Plot_ID, Visit_Year)
+  arrange(Plot_ID, Start_Date)
 
+# Calculate species trends
 plot_trend <- arches_counts %>%
+  mutate(Visit_Year = year(Start_Date)) %>%
   group_by(Plot_ID, Species, Scientific_Name) %>%
   summarize(
     First_yr_cover_pct = Cover_pct[Visit_Year == min(Visit_Year)],
@@ -61,3 +77,26 @@ plot_trend <- arches_counts %>%
     .groups = 'drop'
   ) %>%
   left_join(tlu_plants, by = join_by(Species == Master_Plant_Code))
+
+
+### 
+seug_taxa <- read_csv(here('data/taxaList.csv')) %>%
+  select(taxaCode:scientificName_ITIS)
+
+seug_arches <- read_csv(here('data/SEUG_vegData.csv')) %>%
+  filter(str_starts(locationID, 'A'), year(eventDate) >= 2010) %>%
+  select(c(locationID, eventDate, taxaCode, coverValue))
+
+arches_seug_taxa <- arches_counts %>%
+  left_join(seug_taxa, by = join_by(Scientific_Name == scientificName_ITIS)) %>%
+  left_join(seug_taxa, by = join_by(Scientific_Name == scientificName_SEUG)) %>%
+  mutate(Taxa_Code = if_else(is.na(taxaCode.x), taxaCode.y, taxaCode.x)) %>%
+  select(-c(taxaCode.x:scientificName_ITIS))
+
+arches_locID <- arches_seug_taxa %>%
+  left_join(seug_arches, by = join_by(Start_Date == eventDate, 
+                                      Taxa_Code == taxaCode,
+                                      Cover_Class == coverValue)) %>%
+  mutate(Visit_Year = year(Start_Date)) %>%
+  distinct(Visit_Year, Plot_ID, locationID)
+            
